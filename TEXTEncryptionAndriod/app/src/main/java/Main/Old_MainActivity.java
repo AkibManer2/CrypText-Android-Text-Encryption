6655f5a3d5ea96e4e24d09b868b0640478b14baa
Akib Maner <akibmnaer7865.2@gmail.com>
2023-11-02 12:03:59 +0530

    Upload

---
+++ TEXTEncryptionAndriod/app/src/main/java/Main/Old_MainActivity.java
@@ -0,0 +1,39 @@
+package Main;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.Algorithms.R;
+
+
+
+public class Old_MainActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.encryption_main);
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
